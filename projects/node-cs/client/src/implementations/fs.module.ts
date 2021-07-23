15775de8d50e5fddadf1a072b4fda958028a6df4
FloydCrimson <36471780+FloydCrimson@users.noreply.github.com>
2021-07-23 14:06:31 +0200

    Added access and writeFile

--- projects/node-cs/client/src/implementations/fs.module.ts
+++ projects/node-cs/client/src/implementations/fs.module.ts
@@ -1,4 +1,5 @@
 export interface FSModule {
+    access(path: string, mode?: AccessConstants): Promise<void>;
     lstat(path: string, options?: { bigint?: false; throwIfNoEntry?: boolean; }): Promise<Stats>;
     lstat(path: string, options: { bigint: true; throwIfNoEntry?: boolean; }): Promise<BigIntStats>;
     lstat(path: string, options?: { bigint?: boolean; throwIfNoEntry?: boolean; }): Promise<Stats | BigIntStats>;
@@ -6,9 +7,20 @@ export interface FSModule {
     readdir(path: string, options: { encoding?: BufferEncoding; withFileTypes: true; } | BufferEncoding): Promise<Dirent[]>;
     readdir(path: string, options?: { encoding?: BufferEncoding; withFileTypes?: boolean; } | BufferEncoding): Promise<string[] | Dirent[]>;
     readFile(path: string, options: { encoding: BufferEncoding; flag?: string; } | string): Promise<string>;
+    writeFile(path: string, data: string, options: WriteFileOptions): Promise<void>;
 }
 
 export type BufferEncoding = 'ascii' | 'utf8' | 'utf-8' | 'utf16le' | 'ucs2' | 'ucs-2' | 'base64' | 'base64url' | 'latin1' | 'binary' | 'hex';
+
+export type AccessConstants = 'UV_FS_SYMLINK_DIR' | 'UV_FS_SYMLINK_JUNCTION' | 'O_RDONLY' | 'O_WRONLY' | 'O_RDWR' | 'UV_DIRENT_UNKNOWN' | 'UV_DIRENT_FILE' | 'UV_DIRENT_DIR' | 'UV_DIRENT_LINK' | 'UV_DIRENT_FIFO' | 'UV_DIRENT_SOCKET' | 'UV_DIRENT_CHAR' | 'UV_DIRENT_BLOCK' | 'S_IFMT' | 'S_IFREG' | 'S_IFDIR' | 'S_IFCHR' | 'S_IFLNK' | 'O_CREAT' | 'O_EXCL' | 'UV_FS_O_FILEMAP' | 'O_TRUNC' | 'O_APPEND' | 'F_OK' | 'R_OK' | 'W_OK' | 'X_OK' | 'UV_FS_COPYFILE_EXCL' | 'COPYFILE_EXCL' | 'UV_FS_COPYFILE_FICLONE' | 'COPYFILE_FICLONE' | 'UV_FS_COPYFILE_FICLONE_FORCE' | 'COPYFILE_FICLONE_FORCE';
+
+export type Mode = number | string;
+
+export type WriteFileOptions = (ObjectEncodingOptions & { mode?: Mode | undefined; flag?: string | undefined; }) | string | null;
+
+export interface ObjectEncodingOptions {
+    encoding?: BufferEncoding | null | undefined;
+}
 
 export interface Stats extends StatsBase<number> { }
 
